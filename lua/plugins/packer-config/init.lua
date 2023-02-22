306c287729ea4a0ad3a41b4e6192179147eecb0c
rts4 <mapfg.rts4@techhh.ca>
2023-02-22 15:08:36 -0500

    fix broken packer config - rts4

--- lua/plugins/packer-config/init.lua
+++ lua/plugins/packer-config/init.lua
@@ -2,7 +2,7 @@ vim.cmd([[packadd packer.nvim]])
 vim.cmd([[
     augroup packer_user_config
         autocmd!
-        autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+        autocmd BufWritePost init.lua source <afile> | PackerSync
     augroup end
 ]])
 
