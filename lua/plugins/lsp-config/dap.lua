ec11d9d6223ece5f85b45565c4b2c675e4822744
rts4 <mapfg.rts4@techhh.ca>
2023-02-28 13:39:45 -0500

    add a few more debuggers - rts4

--- lua/plugins/lsp-config/dap.lua
+++ lua/plugins/lsp-config/dap.lua
@@ -24,3 +24,75 @@ dap.configurations.javascript = {
 		processId = require("dap.utils").pick_process,
 	},
 }
+dap.configurations.typescript = {
+	{
+		name = "Launch",
+		type = "node2",
+		request = "launch",
+		program = "${file}",
+		cwd = vim.fn.getcwd(),
+		sourceMaps = true,
+		protocol = "inspector",
+		console = "integratedTerminal",
+	},
+	{
+		name = "Attach to process",
+		type = "node2",
+		request = "attach",
+		processId = require("dap.utils").pick_process,
+	},
+}
+
+-- Delve (Go)
+dap.adapters.delve = {
+	type = "server",
+	port = "${port}",
+	executable = {
+		command = "dlv",
+		args = { "dap", "-l", "localhost:${port}" },
+	},
+}
+dap.configurations.go = {
+	{
+		type = "delve",
+		name = "Debug",
+		request = "launch",
+		program = "${file}",
+	},
+	{
+		type = "delve",
+		name = "Debug test file",
+		request = "launch",
+		mode = "test",
+		program = "${file}",
+	},
+	{
+		type = "delve",
+		name = "Debug test file (go.mod)",
+		request = "launch",
+		mode = "test",
+		program = "./${relativeFileDirname}",
+	},
+}
+
+-- Elixir
+dap.adapters.mix_task = {
+	type = "executable",
+	command = os.getenv("HOME") .. "/.local/share/nvim/mason/packages/elixir-ls/debugger.sh",
+	args = {},
+}
+dap.configurations.elixir = {
+	{
+		type = "mix_task",
+		name = "mix test",
+		task = "test",
+		taskArgs = { "--trace" },
+		request = "launch",
+		startApps = true,
+		projectDir = "${workspaceFolder}",
+		requireFiles = {
+			"test/**/test_helper.exs",
+			"test/**/*_test.exs",
+		},
+	},
+}
